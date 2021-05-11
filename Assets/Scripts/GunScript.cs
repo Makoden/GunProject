448ae7728d0c93f69f4eb60ba1e209b2a00dc63f
Makoden <chrisblanchard3@gmail.com>
2021-05-11 17:55:52 -0400

    Animations

--- Assets/Scripts/GunScript.cs
+++ Assets/Scripts/GunScript.cs
@@ -9,11 +9,14 @@ public class GunScript : MonoBehaviour
 
     [SerializeField]
     LayerMask layerMask;
-   
+
+    
+     private Animator gunFireAnim;
     // Start is called before the first frame update
     void Start()
     {
         line = GetComponent<LineRenderer>();
+        gunFireAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -23,25 +26,28 @@ public class GunScript : MonoBehaviour
         {
             Ray ray = new Ray(transform.position, transform.forward);
             RaycastHit hit;
-
+            gunFireAnim.SetBool("shootGun", true);
             if (Physics.Raycast(ray, out hit, rayLength, layerMask))
             {
                 line.startColor = Color.blue;
+                
             }
 
             else
             {
                 line.startColor = Color.grey;
+               
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.G) || OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        if (Input.GetKeyUp(KeyCode.G) || OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
         {
             line.startColor = Color.white;
+            gunFireAnim.SetBool("shootGun", false);
         }
 
         line.SetPosition(0, transform.position);
-        line.SetPosition(1, transform.position, transform.forward * rayLength);
-           
+        line.SetPosition(1, transform.position + (transform.forward * rayLength));
+          
     }
 }
